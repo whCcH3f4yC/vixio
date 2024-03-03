ffd565f0d6911709faa37934f7b7bcf496e7da69
whCcH3f4yC <roni.ribeiro@hotmail.com>
2024-03-03 13:34:10 -0300

    add logo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/js/bootstrap.min";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/App.css"
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
