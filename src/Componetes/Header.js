e57b0dae4ce5f8ba4863f73daeeca1544558830c
whCcH3f4yC <roni.ribeiro@hotmail.com>
2024-03-03 13:37:11 -0300

    carousel auto

--- src/Componetes/Header.js
+++ src/Componetes/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <div className=' container-fluid p-0 '>
             
                 <h1 className='text-center logo '>vixio</h1>
-                <div id="carouselBanner" className="carousel slide" >
+                <div id="carouselBanner" className="carousel slide" data-bs-ride="carousel"> 
                     <div className="carousel-indicators">
                         <button type="button" data-bs-target="#carouselBanner" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselBanner" data-bs-slide-to="1" aria-label="Slide 2"></button>
