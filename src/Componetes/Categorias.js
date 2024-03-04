fc69882252cf1f04d1664599832ee0e654b8243c
whCcH3f4yC <roni.ribeiro@hotmail.com>
2024-03-04 19:17:53 -0300

    add modal cart

--- src/Componetes/Categorias.js
+++ src/Componetes/Categorias.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Categorias.module.css';
 import bag from '../Image/bag.png'
+import ModalCart from "./ModalCart";
 const Categorias = ({ onCategoriaSelecionada }) => {
+
+    const [open, setOpen] = useState(false)
     return (
         <div className='d-flex bg-white align-items-center '>
         <nav id={style.containerNav} className={'container-fluid '}>
             <div className={`row p-3`}>
                 <ul className={`${style.NavList}`}>
-                    <li><a href="home" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada(null); }}>Home</a></li>
-                    <li><a href="electronics" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada('electronics'); }}>Electronics</a> </li>
-                    <li><a href="jewelery" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada('jewelery'); }}>Jewelery</a> </li>
-                    <li><a href="men's clothin" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada("men's clothing"); }}>Men's Clothin</a> </li>
-                    <li><a href="women's clothing" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada("women's clothing"); }}>Women's Clothing</a> </li>
+
+                    <li>
+                        <a href="home" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada(null); }}>Home</a>
+                    </li>
+
+                    <li>
+                        <a href="electronics" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada('electronics'); }}>Electronics</a>
+                    </li>
+
+                    <li>
+                        <a href="jewelery" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada('jewelery'); }}>Jewelery</a>
+                    </li>
+
+                    <li>
+                        <a href="men's clothin" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada("men's clothing"); }}>Men's Clothin</a>
+                    </li>
+
+                    <li>
+                        <a href="women's clothing" onClick={(e) => { e.preventDefault(); onCategoriaSelecionada("women's clothing"); }}>Women's Clothing</a>
+                    </li>
+
                 </ul>
             </div>
         </nav>
         <div className={style.containerIgm}>
-            <img className={style.img} src={bag} alt="" />
+            <img className={style.img} src={bag} alt="carrinho" onClick={()=> setOpen(!open)} />
         </div>
+
+            <ModalCart isOpen={open} setOpen={setOpen} />
         </div>
     );
 };
